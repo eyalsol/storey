37311e3f9b689e75f99270b1575363f053009f65
Eyal Salomon <eyals@iguazio.com>
2021-08-26 16:22:43 +0300

    - Support for sample per event's key on `SampleWindow`
    Signed-off-by: Eyal Salomon <eyals@iguazio.com>

--- storey/steps/sample.py
+++ storey/steps/sample.py
@@ -1,7 +1,8 @@
 from enum import Enum
 
 from storey import Flow
-from storey.dtypes import _termination_obj
+from storey.dtypes import Event, _termination_obj
+from typing import Optional, Union, Callable
 
 
 class EmitPeriod(Enum):
@@ -22,6 +23,12 @@ class SampleWindow(Flow):
     Available options:
         2.1) True - The last event seen will be emitted downstream.
         2.2) False - The last event seen will NOT be emitted downstream.
+    :param count_per_key: Whether to sample up to window size on per key basis. False samples on all events.
+    :param key: The key by which events are sampled. By default (None), events are not sampled by key.
+        Other options may be:
+        Set a '$key' to sample events by the Event.key property.
+        set a 'str' key to sample events by Event.body[str].
+        set a Callable[Any, Any] to sample events by a custom key extractor.
     """
 
     def __init__(
@@ -29,6 +36,9 @@ class SampleWindow(Flow):
         window_size: int,
         emit_period: EmitPeriod = EmitPeriod.FIRST,
         emit_before_termination: bool = False,
+        count_per_key: bool = False,
+        key_selector=None,
+        key: Optional[Union[str, Callable[[Event], str]]] = None,
         **kwargs,
     ):
         super().__init__(full_event=True, **kwargs)
@@ -42,9 +52,26 @@ class SampleWindow(Flow):
         self._window_size = window_size
         self._emit_period = emit_period
         self._emit_before_termination = emit_before_termination
-
+        self._count_per_key = count_per_key
+        self._key_selector = key_selector
+        self._per_key_count = dict()
         self._count = 0
         self._last_event = None
+        self._extract_key: Optional[Callable[[Event], str]] = self._create_key_extractor(key)
+
+    @staticmethod
+    def _create_key_extractor(key) -> Callable:
+        if key is None:
+            return lambda event: None
+        elif callable(key):
+            return key
+        elif isinstance(key, str):
+            if key == '$key':
+                return lambda event: event.key
+            else:
+                return lambda event: event.body[key]
+        else:
+            raise ValueError(f'Unsupported key type {type(key)}')
 
     async def _do(self, event):
         if event is _termination_obj:
@@ -52,21 +79,35 @@ class SampleWindow(Flow):
                 await self._do_downstream(self._last_event)
             return await self._do_downstream(_termination_obj)
         else:
-            self._count += 1
+            key = self._extract_key(event)
+
+            if self._count_per_key and key is not None:
+                if key not in self._per_key_count:
+                    self._per_key_count[key] = 1
+                else:
+                    self._per_key_count[key] += 1
+                count = self._per_key_count[key]
+            else:
+                self._count += 1
+                count = self._count
 
             if self._emit_before_termination:
                 self._last_event = event
 
-            if self._should_emit():
+            if self._should_emit(count):
                 self._last_event = None
                 await self._do_downstream(event)
 
-            if self._count == self._window_size:
-                self._count = 0
+            if count == self._window_size:
+                if self._count_per_key and key is not None:
+                    self._per_key_count[key] = 0
+                else:
+                    self._count = 0
 
-    def _should_emit(self):
-        if self._emit_period == EmitPeriod.FIRST and self._count == 1:
+    def _should_emit(self, count):
+        if self._emit_period == EmitPeriod.FIRST and count == 1:
             return True
-        elif self._emit_period == EmitPeriod.LAST and self._count == self._window_size:
+        elif self._emit_period == EmitPeriod.LAST and count == self._window_size:
             return True
         return False
+
